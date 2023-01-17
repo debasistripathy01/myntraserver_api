0ea6cca22aa851decbcf9764cfe4e9775f3f873a
debasistripathy01 <debasistripathy04@gmail.com>
2023-01-17 11:57:33 +0530

    server_myntra

---
+++ db.js
@@ -0,0 +1,22 @@
+const dealofday = require("./myntraServer/dealofday.json")
+const bestofmyntra = require("./myntraServer/bestofmyntra.json")
+const cart = require("./myntraServer/cart.json")
+const categoriestobag = require("./myntraServer/categoriestobag.json")
+const dealsonlatestarrival = require("./myntraServer/dealsonlatestarrival.json")
+const gitftingcards = require("./myntraServer/gitftingcards.json")
+const men = require("./myntraServer/men.json")
+const newintopbrands = require("./myntraServer/newintopbrands.json")
+const products = require("./myntraServer/products.json")
+const toppicks = require("./myntraServer/toppicks.json")
+const women = require("./myntraServer/women.json")
+
+const total = require("./db.json");
+
+
+module.exports={
+    dealofday,
+    bestofmyntra,
+    cart,categoriestobag, dealsonlatestarrival, gitftingcards, 
+    men,
+    newintopbrands, products, toppicks, total
+}
